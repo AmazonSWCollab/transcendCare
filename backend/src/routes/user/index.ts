70f2f42e7b98b3d2dd3fdac210cbbf590f47a60d
oscisn93 <ocisneros1@csu.fullerton.edu>
2023-04-20 00:23:57 -0700

    Added get and patch methods to user api as well as accounted for missing/invalid cities (naively)

--- backend/src/routes/user/index.ts
+++ backend/src/routes/user/index.ts
@@ -10,20 +10,37 @@ interface UserInfo {
 }
 
 const example: FastifyPluginAsync = async (fastify, opts): Promise<void> => {
-  fastify.get("/users/:id", async function (request, reply) {
-    return "this is an example";
-  });
+  // get user by id
+  fastify.get<{ Params: { id: number } }>(
+    "/users/:id",
+    async function (request, reply) {
+      const { id } = request.params;
+      const userSelect = await fastify.db
+        .select()
+        .from(users)
+        .where(eq(users.id, id));
+      reply.status(200).send({ user: userSelect[0] });
+    }
+  );
+
   // create new user
   fastify.post<{
     Body: UserInfo;
-  }>("/user", async (request, reply) => {
+  }>("/user/create", async (request, reply) => {
     const { fullName, phone, city } = request.body;
     // get the id of the city the user names
-    // TODO: account for  invalid cities
     const citySelect = await fastify.db
       .select()
       .from(cities)
       .where(eq(cities.name, city));
+    // Account for  invalid city names
+    if (citySelect.length < 1) {
+      const insertCity = await fastify.db
+        .insert(cities)
+        .values({ name: city })
+        .returning();
+      citySelect.push(insertCity[0]);
+    }
     // create new user & insert
     const user: NewUser = {
       fullName: fullName,
@@ -34,6 +51,65 @@ const example: FastifyPluginAsync = async (fastify, opts): Promise<void> => {
     };
     const response = await fastify.db.insert(users).values(user).returning();
     reply.status(201).send({ user: response[0] });
+  });
+
+  // update user fullname
+  fastify.patch<{
+    Body: { full_name: string };
+    Params: { id: number };
+  }>("/user/:id/update/name", async (request, reply) => {
+    const { full_name } = request.body;
+    const user_id = request.params.id;
+    const user = await fastify.db
+      .update(users)
+      .set({ fullName: full_name, updatedAt: new Date() })
+      .where(eq(users.id, user_id))
+      .returning();
+    reply.status(200).send({ user: user[0] });
+  });
+
+  // update user phone
+  fastify.patch<{
+    Body: { phone_number: string };
+    Params: { id: number };
+  }>("/user/:id/update/phone", async (request, reply) => {
+    const { phone_number } = request.body;
+    const user_id = request.params.id;
+    const user = await fastify.db
+      .update(users)
+      .set({ phone: phone_number, updatedAt: new Date() })
+      .where(eq(users.id, user_id))
+      .returning();
+    reply.status(200).send({ user: user[0] });
+  });
+
+  // update user city
+  fastify.patch<{
+    Body: { city_name: string };
+    Params: { id: number };
+  }>("/user/:id/update/city", async (request, reply) => {
+    const { city_name } = request.body;
+    const user_id = request.params.id;
+    // get the id of the city the user names
+    const citySelect = await fastify.db
+      .select()
+      .from(cities)
+      .where(eq(cities.name, city_name));
+    // Account for  invalid city names
+    if (citySelect.length < 1) {
+      const insertCity = await fastify.db
+        .insert(cities)
+        .values({ name: city_name })
+        .returning();
+      citySelect.push(insertCity[0]);
+    }
+    // Now update user
+    const user = await fastify.db
+      .update(users)
+      .set({ cityId: citySelect[0].id, updatedAt: new Date() })
+      .where(eq(users.id, user_id))
+      .returning();
+    reply.status(200).send({ user: user[0] });
   });
 };
 
