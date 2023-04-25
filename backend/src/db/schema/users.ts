2466590441b012e045d5969f75b63e28b91c4605
Oscar Cisneros <41174627+oscisn93@users.noreply.github.com>
2023-04-25 16:26:48 -0700

    Updated User Model
    Made self identification information optional, and refactored options on identify attributes

--- backend/src/db/schema/users.ts
+++ backend/src/db/schema/users.ts
@@ -13,8 +13,7 @@ export const roleEnum = pgEnum("role", ["user", "admin"]);
 
 export const identityEnum = pgEnum("identity", [
   "non-binary",
-  "female",
-  "male",
+  "transgender"
   "other",
 ]);
 
@@ -30,15 +29,14 @@ export const users = pgTable("users", {
   firstName: varchar("full_name", { length: 256 }).notNull(),
   lastName: varchar("last_name", { length: 256 }).notNull(),
   preferredName: varchar("preferred_name", { length: 256 }),
-  phone: varchar("phone", { length: 10 }).notNull(),
   role: roleEnum("role").default("user").notNull(),
   cityId: integer("city_id").references(() => cities.id),
   dateOfBirth: date("date_of_birth", { mode: "string" }),
   createdAt: timestamp("created_at").defaultNow().notNull(),
   updatedAt: timestamp("updated_at").defaultNow().notNull(),
-  identity: identityEnum("identity").default("other").notNull(),
-  otherIdentity: varchar("other_identity", { length: 128 }),
-  pronouns: pronounsEnum("pronouns").default("custom").notNull(),
+  identity: identityEnum("identity"),
+  otherIdentity: varchar("other_identity", { length: 64 }),
+  pronouns: pronounsEnum("pronouns"),
   customPronouns: varchar("custom_pronouns", { length: 32 }),
 });
 
