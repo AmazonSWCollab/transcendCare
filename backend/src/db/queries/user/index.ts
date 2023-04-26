a919efc60aeb38e6d4d863850771675d26c6aa86
oscisn93 <ocisneros1@csu.fullerton.edu>
2023-04-25 20:56:57 -0700

    Added more prisma queries

--- backend/src/db/queries/user/index.ts
+++ backend/src/db/queries/user/index.ts
@@ -1,14 +1,32 @@
 import { db } from '../db';
 
-export async fucntion findUnique(id): Promise<User | null> {
+export async function findUnique(id: number): Promise<User | null> {
 	const users = await db.select().from(users).where(eq(users.id, id));
 	if (users.length < 1) {
-		throw Error(`User does not exist with id: ${id}.`);
+	    return null;
 	}
-	const user = users[0];
-	retrurn user;
+	retrurn users[0];
 }
 
 export async function createNewUser(firstName, lastName): Promise<User | null> {
-	const users = await db.insert(users).values({ first_name: firstName, last_name: lastName }).returning();
+	const users = await db.insert(users).values({ firstName: firstName, lastName: lastName }).returning();
+	if (users.length < 1) {
+		return null;
+	}
+	return users[0];
 }
+
+export async function patchUserName(id, firstName, lastName) {
+	const users = await db.update(users).where(eq(users.id, id)).values({ users.firstName: firstName, users.lastName: lastName }).returning();
+	if (users.length < 1) {
+		return null;
+	}
+	return users[0];
+}
+
+export async function pathUserPreferredName(id, preferredName) {
+	const users = await db.update(users).where(eq(users.id, id)).values({ users.preferredName: preferredName }).returning();
+	if (users.length < 1) return null;
+	return users[0];
+}
+
