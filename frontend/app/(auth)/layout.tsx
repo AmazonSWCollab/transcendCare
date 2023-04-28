56301532796c76b9521de771e2190ac16a6a09b1
Daniel Truong <aduy1122@gmail.com>
2023-04-28 03:56:18 -0700

    make it scroll

--- frontend/app/(auth)/layout.tsx
+++ frontend/app/(auth)/layout.tsx
@@ -10,7 +10,7 @@ export default function AuthLayout({
   children: React.ReactNode;
 }) {
   return (
-    <section className="bg-pink_bg px-10 flex flex-col min-h-screen justify-center items-center">
+    <section className="bg-pink_bg px-10 flex flex-col min-h-screen overflow-hidden justify-center items-center">
       {children}
     </section>
   );
