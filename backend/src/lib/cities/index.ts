b32132cc8364e723cdcd952316ab5f778c48864d
oscisn93 <ocisneros1@csu.fullerton.edu>
2023-04-29 02:47:17 -0700

    updated database accessors as well as some udpates to routes

--- backend/src/lib/cities/index.ts
+++ backend/src/lib/cities/index.ts
@@ -1,8 +1,8 @@
 import { eq } from "drizzle-orm";
 import { db } from "../../db";
-import { cities } from "../../db/schema/cities";
+import { cities, City } from "../../db/schema/cities";
 
-export async function getCity(cityName: string) {
+export async function getCity(cityName: string): Promise<City | null> {
   const citySelect = await db
     .select()
     .from(cities)
@@ -10,15 +10,19 @@ export async function getCity(cityName: string) {
     .all();
   // Account for  invalid city name
   if (citySelect.length < 1) {
-    return createCity(cityName);
+    return createCity(cityName);j
   }
   return citySelect[0];
 }
 
-export async function createCity(cityName: string) {
+export async function createCity(cityName: string, cityLongitude: string, cityLatitude: string) {
   const insertCity = await db
     .insert(cities)
-    .values({ name: cityName })
+    .values({
+      name: cityName,
+      longitude: cityLongitude,
+      latitude: cityLatitude
+    })
     .returning()
     .all();
 
@@ -27,3 +31,18 @@ export async function createCity(cityName: string) {
   }
   return insertCity[0];
 }
+
+export async function updateCityName(cityName: string, newName: string) {
+  const updatedCity = await db
+    .update(cities)
+    .set({ name: newName })
+    .where(eq(cities.name, cityName))
+    .returning()
+    .all();
+  if (!updatedCity) {
+    return null;
+  }
+
+  return updatedCity[0];
+}
+
--- backend/src/lib/profile/index.ts
+++ backend/src/lib/profile/index.ts
@@ -3,50 +3,55 @@ import { profiles, Profile } from "../../db/schema/profile";
 import { eq } from "drizzle-orm";
 import { getCity } from "../cities";
 
-export async function findUnique(id: string): Promise<Profile | null> {
-  const user = await db.select().from(profiles).where(eq(profiles.userId, id)).all();
+/**
+ * This file contains all database accessor functions
+ * we want them all in one place so we can (slightly more) 
+ * easily migrate databases
+ */
 
-  if (user.length !== 1) {
+export async function findUnique(id: string): Promise<Profile | null> {
+  const profile = await db.select().from(profiles).where(eq(profiles.userId, id)).all();
+
+  if (profile.length !== 1) {
     return null;
   }
-  return user[0];
+  return profile[0];
 }
-
 
 export async function patchProfilePreferredName(
   id: string,
   preferredName?: string
 ): Promise<Profile | null> {
-  const user = await db
+  const profile = await db
     .update(profiles)
     .set({ preferredName: preferredName })
     .where(eq(profiles.userId, id))
     .returning()
     .all();
 
-  if (user.length !== 1) {
+  if (profile.length !== 1) {
     return null;
   }
 
-  return user[0];
+  return profile[0];
 }
 
 export async function patchProfileRole(
   id: string,
   role: "user" | "admin"
 ): Promise<Profile | null> {
-  const user = await db
+  const profile = await db
     .update(profiles)
     .set({ role: role })
     .where(eq(profiles.userId, id))
     .returning()
     .all();
 
-  if (user.length !== 1) {
+  if (profile.length !== 1) {
     return null;
   }
 
-  return user[0];
+  return profile[0];
 }
 
 export async function patchProfileCity(
@@ -55,61 +60,52 @@ export async function patchProfileCity(
 ): Promise<Profile | null> {
   const city = await getCity(cityName);
 
-  const user = await db
+  const profile = await db
     .update(profiles)
     .set({ cityId: city?.id })
     .where(eq(profiles.userId, id))
     .returning().
     all();
 
-  if (user.length !== 1) {
+  if (profile.length !== 1) {
     return null;
   }
 
-  return user[0];
+  return profile[0];
 }
 
 export async function patchProfileIdentity(
   id: string,
   identity: string,
 ): Promise<Profile | null> {
-  const user = await db
+  const profile = await db
     .update(profiles)
     .set({ identity: identity })
     .where(eq(profiles.userId, id))
     .returning()
     .all();
 
-  if (user.length !== 1) {
+  if (profile.length !== 1) {
     return null;
   }
 
-  return user[0];
+  return profile[0];
 }
 
 export async function patchProfilePronouns(
   id: string,
-  pronouns:
-    | "they/them/theirs"
-    | "she/her/hers"
-    | "he/him/his"
-    | "custom"
-    | null,
-  customPronouns: string | null
+  pronouns: string,
 ): Promise<Profile | null> {
-  if (pronouns !== "custom") {
-    customPronouns = null;
-  }
-  const user = await db
+  const profile = await db
     .update(profiles)
     .set({ pronouns: pronouns })
     .where(eq(profiles.userId, id))
     .returning()
     .all();
 
-  if (user.length !== 1) {
+  if (profile.length !== 1) {
     return null;
   }
 
-  return user[0];
+  return profile[0];
 }
