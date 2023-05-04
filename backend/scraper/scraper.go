a57c2779ee0a1694b87f1c08b496c280156e3bb5
oscisn93 <ocisneros1@csu.fullerton.edu>
2023-05-04 01:28:26 -0700

    added geocordinates to providers

--- backend/scraper/scraper.go
+++ backend/scraper/scraper.go
@@ -1,65 +1,137 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
+	"io"
 	"log"
-  "net/http"
-  "encoding/json"
-  
+	"net/http"
+	"net/url"
+	"os"
+
 	"github.com/gocolly/colly/v2"
+	"github.com/gofiber/fiber/v2"
+	"github.com/joho/godotenv"
 )
 
-type provider struct {
-  Title string
-  Address string
+func init() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
 }
 
-func handler(w http.ResponseWriter, r *http.Request) {
-  URL := "https://www.circlemedical.com/circle-locations"
+type provider struct {
+	Title    string
+	Address  string
+	Location []float32
+}
 
-  c := colly.NewCollector(
+type Property struct {
+	Accuracy  string `json:"accuracy"`
+	Mapbox_id string `json:"mapbox_id"`
+}
+
+type GeoCoordinates struct {
+	Type        string    `json:"type"`
+	Coordinates []float32 `json:"coordinates"`
+}
+
+type Context struct {
+	Id        string `json:"id"`
+	Mapbox_id string `json:"mapbox_id"`
+	Text      string `json:"text"`
+}
+
+type Feature struct {
+	Id         string         `json:"id"`
+	Type       string         `json:"type"`
+	PlaceType  []string       `json:"place_type"`
+	Relevance  float32        `json:"relevance"`
+	Properties Property       `json:"properties"`
+	Text       string         `json:"text"`
+	PlaceName  string         `json:"place_name"`
+	Center     []float32      `json:"center"`
+	Geometry   GeoCoordinates `json:"geometry"`
+	Address    string         `json:"address"`
+	Contexts   []Context      `json:"context"`
+}
+
+func handler(w *fiber.Ctx) error {
+	URL := "https://www.circlemedical.com/circle-locations"
+
+	c := colly.NewCollector(
 		colly.AllowedDomains("www.circlemedical.com", "circlemedical.com"),
+		colly.CacheDir(".colly.cache"),
 	)
-  
-  providers := make([]provider, 0)
-	
-  c.OnHTML("a.crm-location-item.w-inline-block", func(e *colly.HTMLElement) {
-    p := provider{}
-    p.Title = e.ChildText("h1.location")
-    e.ForEach("div.locationspage-icon-text", func(i int, h *colly.HTMLElement) {
-      if (i == 1) {
-        p.Address = h.ChildText("p.paragraph-small")
-        return
-      }
-    })
-    providers = append(providers, p)
-  })
 
-  c.OnResponse(func(r *colly.Response) {
-    log.Println("Response received", r.StatusCode)
-  })
+	providers := make([]provider, 0)
 
-  c.OnError(func(r *colly.Response, err error) {
-    log.Println("Error:", r.StatusCode, err)
-  })
-  
-  c.Visit(URL)
+	c.OnHTML("a.crm-location-item.w-inline-block", func(e *colly.HTMLElement) {
+		p := provider{}
+		// p.Link = e.Attr("href")
+		p.Title = e.ChildText("h1.location")
+		e.ForEach("div.locationspage-icon-text", func(i int, h *colly.HTMLElement) {
+			if i == 1 {
+				p.Address = h.ChildText("p.paragraph-small")
+				return
+			}
+		})
+		escapedSearchText := url.QueryEscape(p.Address)
+		// CHANGE THIS TO YOUR MAPBOX API KEY
+		path := fmt.Sprintf("https://api.mapbox.com/geocoding/v5/mapbox.places/%s.json?access_token=%s", escapedSearchText, os.Getenv("MAPBOXAPI_KEY"))
 
-  b, err := json.Marshal(providers)
-  if err != nil {
-    log.Println("failed to serialize response:", err)
-    return
-  }
-  w.Header().Add("Content-Type", "application/json")
-  w.Write(b)
+		mapbox_response, err := http.Get(path)
+
+		if err != nil {
+			fmt.Print(err.Error())
+			os.Exit(1)
+		}
+
+		geodata, err := io.ReadAll(mapbox_response.Body)
+		if err != nil {
+			log.Fatal(err)
+		}
+		var result struct {
+			Type        string    `json:"type"`
+			Query       []string  `json:"query"`
+			Features    []Feature `json:"features"`
+			Attribution string    `json:"attribuition"`
+		}
+
+		json.Unmarshal(geodata, &result)
+
+		p.Location = result.Features[0].Center
+		providers = append(providers, p)
+	})
+
+	c.OnResponse(func(r *colly.Response) {
+		log.Println("Response received", r.StatusCode)
+	})
+
+	c.OnError(func(r *colly.Response, err error) {
+		log.Println("Error:", r.StatusCode, err)
+	})
+
+	c.Visit(URL)
+
+	b, err := json.Marshal(providers)
+	if err != nil {
+		log.Println("failed to serialize response:", err)
+		return err
+	}
+	return w.Send(b)
 }
 
 func main() {
-  
-  addr := ":4041"
+	app := fiber.New()
 
-  http.HandleFunc("/", handler)
+	app.Use(func(c *fiber.Ctx) error {
+		c.Set("Content-Type", "application/json")
+		return c.Next()
+	})
 
-  log.Println("Listening on", addr)
-  log.Fatal(http.ListenAndServe(addr, nil))
+	app.Get("/api/v1/providers", handler)
+
+	log.Fatal(app.Listen(":4041"))
 }
-
